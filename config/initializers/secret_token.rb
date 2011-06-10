2f40a2b6a18760833dc17a465be6bf2dfdbd2541
Christopher Thielen <cthielen@gmail.com>
2011-06-09 22:57:33 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wavicle::Application.config.secret_token = '999bcc2a4d3d127e31df85966e15c3eeaf641e566952496857c73d7fba021be9d934fbe7d80d28dbad6aa907d0f146eaaf50a19d404524c889b6c966faaf777a'
